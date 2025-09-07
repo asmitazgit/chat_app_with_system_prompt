dcc3642485c18b9fcb6e1ca3508a42a710625569
asmitazgit <106571665+asmitazgit@users.noreply.github.com>
2025-09-07 21:36:19 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class Config:
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY", "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")
+    MODEL_NAME = os.getenv("MODEL_NAME", "openai/gpt-oss-20b")
+
+# Optionally, you can load from a .env file or other config sources
